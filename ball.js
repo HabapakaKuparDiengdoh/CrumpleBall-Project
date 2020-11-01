258e503be9f53bbefc8fe65f37e77e96fa063cc8
HabapakaKuparDiengdoh <71454406+HabapakaKuparDiengdoh@users.noreply.github.com>
2020-11-01 23:10:33 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -17,7 +17,7 @@ this.image=loadImage("paper.png");
       push();
       translate(pos.x, pos.y);
 
-      imageMode(CENTER);
+      imageMode(RADIUS)
       fill(255);
       image(this.image,0, 0, this.r, this.r);
       pop();
