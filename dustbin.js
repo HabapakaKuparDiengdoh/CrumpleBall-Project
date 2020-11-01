05334713463b369f2b459f25c2dcb9827f0de546
HabapakaKuparDiengdoh <71454406+HabapakaKuparDiengdoh@users.noreply.github.com>
2020-11-01 22:58:21 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -6,13 +6,14 @@ class dustbin {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
+        this.image=loadImage("dustbingreen.png");
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;    
-      rectMode(CENTER);
+      imageMode(CENTER);
       fill(255);
-      rect(pos.x,pos.y, this.width, this.height);
+      image(this.image,pos.x,pos.y, this.width, this.height);
 
     }
   }; 
